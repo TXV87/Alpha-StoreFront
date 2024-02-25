4307e683336ea9c3ff4219c1d786a6e56cf300f0
TXV87 <99159839+TXV87@users.noreply.github.com>
2024-02-25 16:42:47 -0500

    Create submitInfo.js

---
+++ Alpha_Storefront/js/submitInfo.js
@@ -0,0 +1,13 @@
+function submitInfo(){
+	let custInfo = new Map();
+	custInfo.set("email", document.getElementByID("email").value.trim());
+	custInfo.set("firstName", document.getElementByID("firstName").value.trim());
+	custInfo.set("lastName", document.getElementByID("lastName").value.trim());
+	let tempPhone = document.getElementById("phoneNumber").value.trim();
+	if(phoneNumber !== ""){
+		custInfo.set"phoneNumber", tempPhone);
+		}
+	custInfo.set("email", document.getElementByID("email").value.trim());
+	custInfo.set("email", document.getElementByID("email").value.trim());
+}
+	
